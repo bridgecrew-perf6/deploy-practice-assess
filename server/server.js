d1aeb1bbf4d490484362da044c6b0497e604333f
Wesley Lo <wesleylo1995@gmail.com>
2022-02-10 21:18:27 -0600

    initial

--- server/server.js
+++ server/server.js
@@ -6,12 +6,12 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static(path.join(__dirname, '../client/')))
+app.use(express.static('public'))
 
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 const port = process.env.PORT || 5050
 
-app.listen(5050,() => console.log('server running on 5050'))
+app.listen(port,() => console.log(`server running on ${port}`))
