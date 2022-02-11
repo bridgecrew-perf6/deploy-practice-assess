cf31fcfb8cd9dc0d2fc944a8e52d75d0f758f34a
Wesley Lo <wesleylo1995@gmail.com>
2022-02-10 21:07:55 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('client'))
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(5050,() => console.log('server running on 5050'))
